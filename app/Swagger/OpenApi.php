53d2ba94282d496bf263de243e8b37d63021cffd
bahare restegari <rastegari.bahareh@gmail.com>
2025-08-26 22:04:52 +0330

    feat: add swagger docs

---
+++ app/Swagger/OpenApi.php
@@ -0,0 +1,11 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *   title="Postal Tracking API",
+ *   version="1.0.0",
+ *   description="API documentation for Postal Tracking"
+ * )
+ */
